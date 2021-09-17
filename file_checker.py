279d428d17d2309ba5fa442749768e786cbbccf6
Matt-Crane <matt.crane@bristol.ac.uk>
2021-09-17 16:33:38 +0100

    Input and test file overhaul
    Changed the file loading + implemented checks for file format.
    Changed test file contents to better reflect real contents.
    General program structure redesign

--- file_checker.py
+++ file_checker.py
@@ -7,7 +7,6 @@ from datetime import datetime
 import os
 
 
-
 FILE_FORMAT = [
     "STUDY_ID",
     "ROW_STATUS",
@@ -29,17 +28,16 @@ FILE_FORMAT = [
     "UKLLC_STATUS",
     "NHS_E_Linkage_Permission",
     "NHS_Digital_Study_Number",
-    "NHS_S_Linkage Permission",
+    "NHS_S_Linkage_Permission",
     "NHS_S_Study_Number",
     "NHS_W_Linkage_Permission",
     "NHS_NI_Linkage_Permission",
-    "NHS_NI_Study Number",
+    "NHS_NI_Study_Number",
     "Geocoding_Permission",
     "ZoeSymptomTracker_Permission",
     "Multiple_Birth",
     "National_Opt_Out"
 ]
-
 
 def file_dialog():
     '''
@@ -66,35 +64,94 @@ def file_dialog():
     return filename
 
 
-def load_file():
+def load_labelled_file(filename):
     '''
-    Loads a CSV file. Formats each line as a dictionary. Saves dictionaries in a list.
-    
+    Loads a CSV file. Read formatting. Formats each line as a dictionary. Saves dictionaries in a list.
     '''
-
-    filename = file_dialog()
-    #filename = "Good.csv" # debugging, hard coded file name (must be in same folder)
-
     data = [] # setup to store csv contents as list of dictionaries
+    with open(filename) as csv_file:    
+        csv_reader = csv.DictReader(csv_file)
+        line_count = 0
+        for row in csv_reader:
+            line_count += 1
+            data.append(row)
 
-    try:
-        with open(filename) as csv_file:
-            
-            csv_reader = csv.DictReader(csv_file, fieldnames = FILE_FORMAT)
-            line_count = 0
-            for row in csv_reader:
-       
-                line_count += 1
-                data.append(row)
-
-            print(f'File contains {line_count} entries.')
-
-    except OSError as e:
-        print(e)
-
+        print(f'File contains {line_count} entries.')            
+    ''' Debugging
     for item in data:
         print(item)
+    '''
     return data
+
+
+def load_unlabelled_file(filename):
+    '''
+    Loads a CSV file. Assume formatted as in specification. Formats each line as a dictionary. Saves dictionaries in a list.
+    '''
+    data = [] # setup to store csv contents as list of dictionaries
+    with open(filename) as csv_file:    
+        csv_reader = csv.DictReader(csv_file, fieldnames = FILE_FORMAT, restkey="Overflow", restval="Underflow")
+        line_count = 0
+        for row in csv_reader:
+            line_count += 1
+            data.append(row)
+            # Check if row contains expected number of variables
+            if "Overflow" in row:
+                error_output("Format Error", "Unexpected number of fields. Expected {}, present {}.".format(len(FILE_FORMAT), len(row["Overflow"])+len(FILE_FORMAT)), str(line_count))
+            if "Underflow" in row.values():
+                error_output("Format Error", "Unexpected number of fields. Expected {}, present {}.".format(len(FILE_FORMAT), len([item for item in row.values() if item != "Underflow"])), str(line_count))
+
+        print(f'File contains {line_count} entries.')            
+
+    ''' Debugging
+    for item in data:
+        print(item)
+    '''
+    return data
+
+def load_file(filename = False):
+    '''
+    Get filename from dialog. Check headers of columns.
+    If all variable names are as expected, load in given format.
+    If some variable names are as expected, error and inform of bad naming
+    If no variable names are present, load assuming format as in specification.
+
+    '''
+
+    if not filename: # if filename has not been passed (would only be for debugging/testing)
+        filename = file_dialog()
+
+    global out_filename
+    out_filename = ("out\\{}_Output_Log".format(filename.split("\\")[-1].split(".")[0]))+STR_TIME
+    
+    try:
+        with open(filename) as csv_file:
+            csv_reader = csv.DictReader(csv_file)
+            headers = csv_reader.fieldnames
+
+            # 1. All headers are the same
+            difference = [x for x in headers if x not in FILE_FORMAT]
+            if difference == [] and len(headers) == len(FILE_FORMAT):
+                data = load_labelled_file(filename)
+
+            # 2. No headers are the same (ie column names not included)
+            elif len(difference) == len(headers):
+                print("Headers not included")
+                data = load_unlabelled_file(filename)
+                error_output("Warning: field names not provided", "Column field names are not explicitly stated. Field name is assumed from position.", [0])
+
+            # 3. Some headers are the same (ie some column names have likely been misnamed)
+            else:
+                print(len(difference), len(FILE_FORMAT), len(headers))
+                error_output("Unrecognised field names", "Column field name(s) {} are not as expected. Unable to continue.".format(", ".join(difference)), [0])
+                # Do not continue program
+                return
+
+    except OSError as e:
+        error_output("Load Error", "Unable to read file")
+        return
+
+    content_checker(data)
 
 
 def check_studyID(input_data):
@@ -159,6 +216,9 @@ def check_max_variables(input_data):
     '''
     pass
 
+def content_checker(input_data):
+    check_studyID(input_data)
+
 
 def error_output(error_type = "Error", message = "Unable to verify file", affected_lines = [] ):
     '''
@@ -173,27 +233,34 @@ def error_output(error_type = "Error", message = "Unable to verify file", affect
 
     curpath = os.path.abspath(os.curdir)
 
+    if not os.path.exists(out_filename):
+        open(out_filename, "w")
 
-    if not os.path.exists(OUT_FILENAME):
-        open(OUT_FILENAME, "w")
+    f = open( out_filename, "a")
 
-    f = open( OUT_FILENAME, "a")
+
     f.write(error_type)
     f.write("\n")
     f.write(message)
+    f.write("--------------------")
     f.close()
 
 
 if __name__ == "__main__":
-    OUT_FILENAME = "out\\Checker_Output_Log"+ datetime.now().strftime("%H%M%S")+".txt"
+    # TODO - uncomment and remove debugging loop
+    #STR_TIME = datetime.now().strftime("%H%M%S")+".txt"
 
-    input_data = load_file()
+    # Debugging:
+    # List test cases to run sequentially (avoids file dialog for sake of speed)
+    test_files = ["Good.csv","Good_unlabelled.csv","Bad_field_names.csv",
+            "EXCEED_FILE1_v1_20210514.csv","UnderVals.csv","OverVals.csv",
+            "StudyID_1.csv", "NullROW_STATUS_1.csv", "NullROW_STATUS_2.csv"]
 
-    # 1:
-    check_studyID(input_data)
+    for filename in test_files:
+        print("Testing file {}".format(filename))
+        STR_TIME = datetime.now().strftime("%H%M%S")+".txt"
+        input_data = load_file(filename)
 
-    error_output("Test Error1", "Test message1", [1,2,3])
-    error_output("Test Error2", "Test message2", [4,5,6])
     '''
     TODO list
     - Make check function:
@@ -204,6 +271,7 @@ if __name__ == "__main__":
         5.	Non-valid Date formats (must be DD/MM/YYYY)
         6.	Out of range values (for constrained fields)
         7.	Max 1024 variables per File (only applicable to File 2)
+        8.  Check expected number of columns (file 1)
 
     - Make custom exception for "bad file"
 
@@ -227,10 +295,18 @@ if __name__ == "__main__":
 
 '''
 Record of test files:
-    Good.csv:               All in order - should pass
-    StudyID_1.csv:          Line 1 and 4 studyID duplicated (and both lines ROW_STATUS = "c") - should fail, more than one row with C
-    StudyID_2.csv:          Line 1 and 4 studyID duplicated (line 4 ROW_STATUS = "C", line 1 = "H") - should pass
-    StudyID_3.csv:          Line 1 and 4 studyID duplicated (line 4 ROW_STATUS = "H", line 1 = "C") - should pass
-    NullROW_STATUS_1.csv    Line 2 ROW_STATUS = "H" - should fail, no row with C
-    NullROW_STATUS_2.csv    Lines 1 and 4 ROW_STATUS = "H" - should fail, no row with C
+    - Format tests
+        Good.csv                        Column names correct - should pass
+        Good_unlabelled.csv             No listed column names, correct ordering - should pass with warning
+        Bad_field_names.csv             selection of incorrect field names - should fail to load
+        EXCEED_FILE1_v1_20210514.csv    Proper file name - should be only file to pass with no errors (others will log errors from bad file name)
+        UnderVals.csv                   (unlabelled) Too few columns - should fail to input
+        OverVals.csv                    (unlabelled) Too many columns - should fail to input
+
+
+    - Content tests 
+        Good.csv                All in order - should pass
+        StudyID_1.csv           Line 6,7 studyID duplicated (and both lines ROW_STATUS = "c") - should fail, more than one row with C
+        NullROW_STATUS_1.csv    Line 6,7 ROW_STATUS = "H" - should fail, no row with C
+        NullROW_STATUS_2.csv    Lines 1,2 and 6,7 ROW_STATUS = "H" - should fail, 2 cases of no row with C
 '''
